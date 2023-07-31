d1a2c1fcf957ce40a40ef3422a2d0e7a944737e9
Pricope Marius-Andrei <pricopemarius33@gmail.com>
2023-07-31 21:32:56 +0300

    create the hero section

--- nutrition.bro/src/app/page.tsx
+++ nutrition.bro/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import NavBar from '@/components/navbar'
+import Hero from '@/components/hero'
 
 export default function Home() {
   return (
     <div>
       <NavBar/>
+      <Hero/>
     </div>
   )
 }
