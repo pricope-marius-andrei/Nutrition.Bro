e051d26d2200019204d6d3b4c08bb76da8bf4183
Pricope Marius-Andrei <pricopemarius33@gmail.com>
2023-07-31 22:00:29 +0300

    create description section and make the navbar to be sticky

--- nutrition.bro/src/app/page.tsx
+++ nutrition.bro/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import NavBar from '@/components/navbar'
 import Hero from '@/components/hero'
+import Description from '@/components/description'
 
 export default function Home() {
   return (
     <div>
       <NavBar/>
       <Hero/>
+      <Description text="Do you want to change your alimentation and you don’t know what choices to do? Here you can calculate your calories and compare food’s nutients, only with a click. NUTRITION.BRO is very usefull tool to manage your diat and calories. "/>
     </div>
   )
 }
--- nutrition.bro/src/components/hero/index.tsx
+++ nutrition.bro/src/components/hero/index.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
                 <h1 className="sm:max-w-3xl font-fredoka-medium lg:text-7xl sm:text-6xl text-5xl pb-5 text-black">Here you’ll find all information about nutrition which you’ll ever need.</h1>
                 <Button name="EXPLORE"/>
             </div>
-            <img className="md:flex hidden ml-44 md:ml-0 lg:w-2/6 sm:w-96 mr-44 md:pt-0 pt-10" src="./hero_img.svg"></img>
+            <img className="flex md:ml-0 lg:w-2/6 sm:w-96 w-72 m-auto  md:pt-0 pt-10" src="./hero_img.svg"></img>
         </div>
     )
 }
