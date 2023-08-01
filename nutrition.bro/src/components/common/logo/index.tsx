2649c415371bc7dd05ca465be9133e40dcbbc6cd
Pricope Marius-Andrei <pricopemarius33@gmail.com>
2023-08-01 15:50:25 +0300

    make some small changes for the navbar and the hero section

--- nutrition.bro/src/components/common/logo/index.tsx
+++ nutrition.bro/src/components/common/logo/index.tsx
@@ -2,10 +2,10 @@ export default function Logo(props:any)
 {
     return (
         <a href="#" className="flex">
-            <img src="./logo.svg" className="cursor-pointer inline-block align-middle w-16"></img>
+            <img src="./logo.svg" className="cursor-pointer inline-block align-middle w-10"></img>
             {
                 props.style == "with-text" &&
-                <div className="flex cursor-pointer my-auto text-black mx-5 text-3xl">
+                <div className="flex cursor-pointer my-auto text-black mx-5 text-2xl">
                     <h1 className="font-fredoka-semi-bold">NUTRITION.</h1>
                     <h1 className=" font-fredoka-regular">BRO</h1>
                 </div>
--- nutrition.bro/src/components/hero/index.tsx
+++ nutrition.bro/src/components/hero/index.tsx
@@ -3,7 +3,7 @@ import Button from "../common/button"
 export default function Hero() {
     return (
         <div className="md:flex sm:place-content-between m-auto lg:py-16 py-10">
-            <div className="sm:ml-44 ml-20 m-auto">
+            <div className="md:ml-44 sm:ml-20 ml-16 m-auto">
                 <h1 className="sm:max-w-3xl font-fredoka-medium lg:text-7xl sm:text-6xl text-5xl pb-5 text-black">Here you’ll find all information about nutrition which you’ll ever need.</h1>
                 <Button name="EXPLORE"/>
             </div>
