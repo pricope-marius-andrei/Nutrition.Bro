df684d623b35e1f9af56dc94b85c410f5fa40fa1
Pricope Marius-Andrei <enjoycoding10@gmail.com>
2024-02-18 09:53:44 +0200

    remove explore button from the home page

--- nutrition.bro/src/components/hero/index.tsx
+++ nutrition.bro/src/components/hero/index.tsx
@@ -8,8 +8,7 @@ export default function Hero() {
         <div className="md:flex md:flex-row flex flex-col w-full md:justify-around justify-center items-center px-20 py-10 font-fredoka-medium heropattern-ilikefood-green-lime/10">
             <div className="w-auto py-20 md:self-start self-center">
                 <h1 className="lg:text-5xl text-4xl pb-5 text-black md:w-2/3 w-full">Here is the place where you find all you need.<br></br> Save your progress from the gym and count your calories.</h1>
-                    <Button isRounded={true} name="EXPLORE"/>
-                </div>
+            </div>
             <img className="flex sm:w-96 w-72" src="./images/hero_img.svg"></img>
         </div>
     )
